dcc6ab2b0595999a86ccc57115a9749ee359b633
shashank934602 <vavilala.shashank780@gmail.com>
2025-10-31 12:25:39 +0530

    added the pages for the website

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactSection from '../components/ContactSection'
+
+const ContactPage = () => {
+  return (
+    <div>
+        <ContactSection/>
+    </div>
+  )
+}
+
+export default ContactPage
---
+++ src/pages/EventsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EventsGallery from '../components/EventsGallery'
+
+const EventsPage = () => {
+  return (
+    <div>
+      <EventsGallery/>
+    </div>
+  )
+}
+
+export default EventsPage
+
