726f51e4216d19b95221551a1cff420fd7656269
shashank934602 <vavilala.shashank780@gmail.com>
2025-10-31 11:34:14 +0530

    added hello in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
     <div className="bg-white">
       <Header />
       <main>
+        <p>hero</p>
         <ScrollingAnnouncementBanner />
         <HeroSection />
         <ClientLogosSection />
